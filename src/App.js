0a20b307c6a0f01b445d8b6ae051fe564322a61a
willian <willian.chris.89@gmail.com>
2022-11-18 10:11:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,75 +1,25 @@
-import React, { Component } from 'react'
-import { Container, Row, Col } from 'reactstrap'
-import ModalForm from './Components/Modals/Modal'
-import DataTable from './Components/Tables/DataTable'
-import { CSVLink } from "react-csv"
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  state = {
-    items: []
-  }
-
-  getItems() {
-    fetch('https://636e5c89b567eed48adb2fb3.mockapi.io/users')
-      .then(response => response.json())
-      .then(items => this.setState({ items }))
-      .catch(err => console.log(err))
-  }
-
-  addItemToState = (item) => {
-    this.setState(prevState => ({
-      items: [...prevState.items, item]
-    }))
-  }
-
-  updateState = (item) => {
-    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
-    const newArray = [
-      ...this.state.items.slice(0, itemIndex),
-      item,
-      ...this.state.items.slice(itemIndex + 1)
-    ]
-    this.setState({ items: newArray })
-  }
-
-  deleteItemFromState = (id) => {
-    const updatedItems = this.state.items.filter(item => item.id !== id)
-    this.setState({ items: updatedItems })
-  }
-
-  componentDidMount() {
-    this.getItems()
-  }
-
-  render() {
-    return (
-      <Container className="App">
-        <Row>
-          <Col>
-            <h1 style={{ margin: "20px 0" }}>CRUD Database</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <CSVLink
-              filename={"db.csv"}
-              color="primary"
-              style={{ float: "left", marginRight: "10px" }}
-              className="btn btn-primary"
-              data={this.state.items}>
-              Download CSV
-            </CSVLink>
-            <ModalForm buttonLabel="Add Item" addItemToState={this.addItemToState} />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
